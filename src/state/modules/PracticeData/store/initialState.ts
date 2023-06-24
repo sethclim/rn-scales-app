8a664c2fd245211386fec5a87db1c2abdcfe67ec
sethclim <sethclim@gmail.com>
2023-06-24 17:46:52 -0400

    Added Dispatch

--- src/state/modules/PracticeData/store/initialState.ts
+++ src/state/modules/PracticeData/store/initialState.ts
@@ -1,17 +1,23 @@
+import { Dispatch } from "react";
 import PracticeData from "../../../../data/Models/PracticeData";
+import { IAction } from "../../../types";
 
 export interface IState {
   error: string;
   loading: boolean;
   currentSessionPracticeData: PracticeData;
-  savingPracticeData : any
+  savingPracticeData : any;
+  practiceDatadispatch: Dispatch<IAction>
 }
 
 const initialState: IState = {
   error: '',
   loading: false,
   currentSessionPracticeData: new PracticeData,
-  savingPracticeData : false
+  savingPracticeData : false,
+  practiceDatadispatch: () => {
+    // Dispatch initial value
+   },
 };
 
 export function GetInitialState(): IState {
