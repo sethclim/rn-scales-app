29ab39da3ccc924ee0a2f1a3ac237fc232a17c1c
sethclim <sethclim@gmail.com>
2023-06-25 19:16:22 -0400

    Grid Y Padding And Inner Margin

--- src/screens/PracticeStats/Graph/GraphBuilder.ts
+++ src/screens/PracticeStats/Graph/GraphBuilder.ts
@@ -44,8 +44,8 @@ const getX = (date : number, scale_X : number, start : number) => {
     return date * scale_X + start;
 }
   
-const getY = (count : number, scale_y : number, height : number) => {
-    return height - (count * scale_y)
+const getY = (count : number, scale_y : number, height : number, start : number) => {
+    return height - (count * scale_y - start)
 }
 
 const createPlot = () : plot =>{
@@ -55,7 +55,7 @@ const createPlot = () : plot =>{
     }
 }
 
-const buildExercisePlots = (data : PracticeData[],start : number,  WIDTH : number, HEIGHT : number, max_x: number, max_y: number) => {
+const buildExercisePlots = (data : PracticeData[],start_x : number, start_y : number,  WIDTH : number, HEIGHT : number, max_x: number, max_y: number) => {
 
 
     const ex : exercises = {
@@ -90,8 +90,8 @@ const buildExercisePlots = (data : PracticeData[],start : number,  WIDTH : numbe
     //Move To
     for(let [exercise, count] of data[0].Counts.entries())
     {
-        const x = getX(data[0].Date.valueOf() - start_time.valueOf(), scale_X, start)
-        const y = getY(count, scale_y, HEIGHT)
+        const x = getX(data[0].Date.valueOf() - start_time.valueOf(), scale_X, start_x)
+        const y = getY(count, scale_y, HEIGHT, start_y)
 
         console.log("X " + x)
         
@@ -104,8 +104,8 @@ const buildExercisePlots = (data : PracticeData[],start : number,  WIDTH : numbe
     {   
       for(let [exercise, count] of data[i].Counts.entries())
       {
-        const x = getX(data[i].Date.valueOf() - start_time.valueOf(), scale_X, start)
-        const y = getY(count, scale_y, HEIGHT)
+        const x = getX(data[i].Date.valueOf() - start_time.valueOf(), scale_X, start_x)
+        const y = getY(count, scale_y, HEIGHT, start_y)
 
         ex[exercise].line.lineTo(x,y);
         ex[exercise].dots.addCircle(x, y, 6)
@@ -116,7 +116,7 @@ const buildExercisePlots = (data : PracticeData[],start : number,  WIDTH : numbe
 }
 
 
-const buildGrid = (start : number, width: number, height: number, max_x: number, max_y: number) => {
+const buildGrid = (start_x : number, start_y : number, width: number, height: number, max_x: number, max_y: number) => {
     //top to bottow div by 7 space
 
     console.log("Grid width " + width)
@@ -129,21 +129,21 @@ const buildGrid = (start : number, width: number, height: number, max_x: number,
     
     for(let i = 0; i < max_x; i++)
     {
-        console.log("Y Width " + (i * scale_x + start))
-        gridLines.moveTo(i * scale_x + start, 0)
-        gridLines.lineTo(i * scale_x + start, height)
+        console.log("Y Width " + (i * scale_x + start_x))
+        gridLines.moveTo(i * scale_x + start_x, start_y)
+        gridLines.lineTo(i * scale_x + start_x, height + start_y)
     }
 
     //const div_y = 5
     const scale_y = height / max_y
 
-    console.log("Start " + start)
+    console.log("Start " + start_x)
     console.log("width " + width)
 
-    for(let i = 1; i < max_y; i++)
+    for(let i = 0; i <= max_y; i++)
     {
-        gridLines.moveTo(start, i * scale_y)
-        gridLines.lineTo(width + start, i * scale_y)
+        gridLines.moveTo(start_x, i * scale_y + start_y)
+        gridLines.lineTo(width + start_x, i * scale_y + start_y)
     }
 
     return gridLines;
@@ -177,24 +177,24 @@ const buildGraph = (data : PracticeData[], WIDTH : number, HEIGHT : number, ID :
     const grid_div    =  GRAPH_INFO[ID].grid_div
     const total_milli =  GRAPH_INFO[ID].total_milli
 
-    
-    const pad_width   = WIDTH - PADDING
-    const inner_width = pad_width - GRID_RIGHT_MARGIN
-    const inner_start = (PADDING / 2 ) + GRID_RIGHT_MARGIN
-    
-    
+    const pad_width     = WIDTH - PADDING
+    const pad_height    = HEIGHT - PADDING
+    const inner_width   = pad_width - GRID_RIGHT_MARGIN
+    const inner_x_start = (PADDING / 2 ) + GRID_RIGHT_MARGIN
+    const inner_height  = pad_height - GRID_BOTTOM_MARGIN
+
     console.log("Full Width " + WIDTH)
     console.log("inner_width " + inner_width)
-    console.log("inner_start " + inner_start)
+    console.log("inner_start " + inner_x_start)
 
-    console.log("inner_width + pad + pad" + (inner_width + inner_start + inner_start))
+    console.log("inner_width + pad + pad" + (inner_width + inner_x_start + inner_x_start))
 
     const max_y = getMaxY(data);
 
     return {
         ID:         ID,
-        exercises : buildExercisePlots(data,inner_start,inner_width, HEIGHT, total_milli, max_y),
-        grid:       buildGrid(inner_start, inner_width, HEIGHT, grid_div, max_y),
+        exercises : buildExercisePlots(data,inner_x_start, PADDING,inner_width, inner_height, total_milli, max_y),
+        grid:       buildGrid(inner_x_start,PADDING, inner_width, inner_height, grid_div, max_y),
         label:      ID
     }
 }
